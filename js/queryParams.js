47107115b749ef5d83a3131bd9e40dbbcf86b449
Johan <joe.affandy@gmail.com>
2024-09-10 23:21:59 +0700

    remove pronounce

--- js/queryParams.js
+++ js/queryParams.js
@@ -1,7 +1,7 @@
 const urlParams = new URLSearchParams(window.location.search);
 const nama = urlParams.get("to") || "";
-const pronoun = urlParams.get("p") || "Bapak/Ibu/Saudara/i";
+const pronoun = urlParams.get("p") || "";
 
 const namaContainer = document.querySelector(".hero h4.guestPerson span");
-namaContainer.innerText = `${pronoun} ${nama},`.replace(/ ,$/, ",");
+namaContainer.innerText = `${pronoun} ${nama},`.replace(/ ,$/, "");
 document.querySelector("#nama").value = nama;
